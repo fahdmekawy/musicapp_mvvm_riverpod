68548a249dcbc80711f8849a20af8124519bdd0d
Fahd Mekawy <40622301+fahdmekawy@users.noreply.github.com>
2024-07-29 13:38:29 +0300

    signup return 201 status code

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -7,11 +7,13 @@ from pydantic_schemas.user_create import UserCreate
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
 
+from pydantic_schemas.user_login import UserLogin
+
 
 router = APIRouter()
 
 
-@router.post("/signup")
+@router.post("/signup",status_code=201)
 def signup_user(user: UserCreate,db: Session = Depends(get_db)):
     # extract the data that coming from request
     user_db = db.query(User).filter(User.email == user.email).first()
@@ -26,5 +28,20 @@ def signup_user(user: UserCreate,db: Session = Depends(get_db)):
     db.add(user_db)
     db.commit()
     db.refresh(user_db)
+
+    return user_db
+
+
+@router.post("/login")
+def login_user(user: UserLogin,db: Session = Depends(get_db)):
+    # check if the user exists in database
+    user_db = db.query(User).filter(User.email == user.email).first()
+    if not user_db:
+        raise HTTPException(status_code=400, detail="User with this email does not exist")
+    # password matching or not
+    is_match = bcrypt.checkpw(user.password.encode(),user_db.password)
+
+    if not is_match:
+        raise HTTPException(status_code=400, detail="Incorrect Password")
 
     return user_db
